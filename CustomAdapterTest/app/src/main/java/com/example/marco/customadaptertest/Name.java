87e9a72eb9ecab26918bec23020918f7fb2988c5
Marco Hernandez <Marco.Hernandez@hotmail.nl>
2016-04-18 13:49:30 +0200

    CustomArrayAdapterTest toegevoegd. Dit bevat een simpele extentie van de ArrayAdapter.

---
+++ CustomAdapterTest/app/src/main/java/com/example/marco/customadaptertest/Name.java
@@ -0,0 +1,16 @@
+package com.example.marco.customadaptertest;
+
+/**
+ * @author Marco Hernandez
+ * Test class containing two strings.
+ */
+public class Name {
+    String firstName;
+    String surname;
+
+    public Name(String firstName, String surname) {
+        this.firstName = firstName;
+        this.surname = surname;
+    }
+
+}
