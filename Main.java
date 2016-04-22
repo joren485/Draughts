ea86edfc0f3ec42e6865d623276333fee48556c1
joren485 <jorenvrancken@gmail.com>
2016-04-22 13:12:56 +0200

    Small test

--- Main.java
+++ Main.java
@@ -13,17 +13,12 @@ public class Main {
 
         b.movePiece(new Tuple(0, 3), new Tuple(1, 4));
 
-        b.movePiece(new Tuple(1, 4), new Tuple(3, 6));
+        //b.movePiece(new Tuple(1, 4), new Tuple(3, 6));
 
         System.out.println(b);
 
-        Move root = b.getLegalMoves(b.getPiece(new Tuple(3, 6)));
+        Move root = b.getLegalMoves(b.getPiece(new Tuple(1, 4)));
 
-
-
-        System.out.println(b);
-
-        System.out.println(b.getPiece(new Tuple(2, 5)).getinChain());
 
     }
 }
