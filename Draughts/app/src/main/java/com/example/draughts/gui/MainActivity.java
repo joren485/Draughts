c0164e47b7fdf16adaea0d4f4c4a4da43652f535
Joren Vrancken <s4593847@ru.nl>
2016-04-28 22:06:55 +0200

    Remove unsused call

--- Draughts/app/src/main/java/com/example/draughts/gui/MainActivity.java
+++ Draughts/app/src/main/java/com/example/draughts/gui/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -19,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
